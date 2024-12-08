85dcd74309c2e697a9c950200050880d2388c7f3
Lucas <me@keyruu.de>
2024-12-08 12:42:19 +0100

    move to different langs and update just file for scala

---
+++ scala/template/dayXX.scala
@@ -0,0 +1,9 @@
+package dayXX
+
+@main
+def part1(path: String): String =
+  return ""
+
+@main
+def part2(path: String): String =
+  return ""
---
+++ scala/template/dayXX.test.scala
@@ -0,0 +1,7 @@
+package dayXX
+
+class Test extends munit.FunSuite:
+  test("part 1"):
+    assertEquals(part1("dayXX.example"), "")
+  test("part 2"):
+    assertEquals(part2("dayXX.example"), "")
